45e160c527f19a002883c5008d02b306325fffab
wutangshaolin74 <you@example.com>
2022-11-23 14:09:10 +0100

    changement

--- requetes_auto_meteoblue.py
+++ requetes_auto_meteoblue.py
@@ -1,5 +1,14 @@
-import csv 
-import os 
 import urllib
 from urllib import parse, request
+import csv 
+import os 
 import requests
+
+def GetUrl (lat, lon):
+    url_base = "http..."
+    parameters = {}
+    
+    #We then stock our data into a file
+    x_coord = str(x)
+    y_coord = str(y)
+    our_file = "Data fie"
