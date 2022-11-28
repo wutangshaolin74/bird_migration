ea1465838d05349630ad53e922b36fe0231a5ba8
wutangshaolin74 <you@example.com>
2022-11-28 20:05:13 +0100

    change

--- getdataT24.py
+++ getdataT24.py
@@ -1,23 +1,28 @@
 import numpy as np
 import csv 
 
-file = open('Oiseau.csv', newline = '')
-
+file = open('Kivi Kuaka [ID_PROG_1099].csv', newline = '')
 csvReader =csv.reader(file, delimiter = ',')
 latitude=[]
 longtiude=[]
+
 for row in csvReader:
     # Traiter la ligne
     longtiude.append((row[3]))
     latitude.append((row[4]))
+
 nlong=len(longtiude)
 nlat=len(latitude)
+
 pointarrivee= [float(latitude[nlat-1]),float(longtiude[nlong-1])]
 pointdepart= [float(latitude[1]),float(longtiude[1])]
+
 long1=pointdepart[0]
 lat1=pointdepart[1]
+
 long2=pointarrivee[0]
 lat2=pointarrivee[1]
+
 L=[str(long1)+"\n",str(lat1)+"\n",str(long2)+"\n",str(lat2)+"\n"]
 file1 = open("data.csv", "w")
 file1.writelines(L)
